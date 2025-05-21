275fa0d1279f25398e9e037c763559b2c38b7481
soyjetto <61566699+soyjetto@users.noreply.github.com>
2025-05-20 21:22:40 -0500

    Update pong-audio.js

--- javascript/pong-audio.js
+++ javascript/pong-audio.js
@@ -55,14 +55,14 @@ soundArray.push(scoreSound);
 export var ambientSound = new soundFile("ambience.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -20; //turn down volume
+ambientSound.player.volume.value = -10; //turn down volume
 
 export var adventureMusic = new soundFile("adventure wining.mp3");
 soundArray.push(adventureMusic);
 adventureMusic.player.loop = true;
-adventureMusic.player.volume.value = -16;
+adventureMusic.player.volume.value = 0;
 
 export var villageMusic = new soundFile("village.mp3");
 soundArray.push(villageMusic);
 villageMusic.player.loop = true;
-villageMusic.player.volume.value = -16;
+villageMusic.player.volume.value = 0;
